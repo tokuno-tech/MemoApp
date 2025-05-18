87771ef68338af99930f608a470b679ebfe3427c
tokuno-tech <tokuno.web.work@gmail.com>
2025-05-18 23:11:19 +0900

    Create Hello Component

---
+++ src/components/Hello.tsx
@@ -0,0 +1,30 @@
+import {  View, Text, StyleSheet, type TextStyle } from 'react-native'
+
+interface Props {
+    children: string
+    bang?: boolean
+    style?: TextStyle
+}
+
+const Hello = (props: Props): React.JSX.Element => {
+    const { children, bang, style } = props
+  return (
+    <View>
+        <Text style={[styles.text, style]}>
+            Hello {children} {bang === true ? '!' : ''}
+        </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    text: {
+        color: '#fff',
+        backgroundColor: 'blue',
+        fontSize: 20,
+        fontWeight: 'bold',
+        padding: 16
+    }
+})
+
+export default Hello
