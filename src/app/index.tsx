71743023d73a9d4e2a0b51844116b101f1c02ee2
tokuno-tech <tokuno.web.work@gmail.com>
2025-05-19 22:42:20 +0900

    stylng header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,10 +3,10 @@ import { View, Text, StyleSheet } from 'react-native'
 const Index = (): React.JSX.Element => {
     return (
         <View style={styles.container}>
-            <View>
-                <View>
-                    <Text>Memo App</Text>
-                    <Text>ログアウト</Text>
+            <View style={styles.header}>
+                <View style={styles.headerInner}>
+                    <Text style={styles.headerTitle}>Memo App</Text>
+                    <Text style={styles.headerRight}>ログアウト</Text>
                 </View>
             </View>
 
@@ -53,12 +53,31 @@ const Index = (): React.JSX.Element => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
+        flex: 1, //画面いっぱいに要素を広げる
+        backgroundColor: '#fff'
+        
+    },
+    header: {
+        backgroundColor: '#467FD3',
+        height: 104,
+        justifyContent: 'flex-end'
+    },
+    headerInner: {
         alignItems: 'center'
     },
-    text: {
-        fontSize: 20
+    headerRight: {
+        position: 'absolute',
+        right: 16,
+        bottom: 16,
+        color: 'rgba(255, 255, 255, 0.7)'
+        
+    },
+    headerTitle: {
+        marginBottom: 8,
+        fontSize: 22,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        color: '#fff'
     }
 })
 
