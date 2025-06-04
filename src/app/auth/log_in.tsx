767eeeb6cd86627de244950e2459fe2f873e8861
tokuno-tech <tokuno.web.work@gmail.com>
2025-06-04 21:08:52 +0900

    logIn機能追加

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -1,15 +1,27 @@
 import {
-    View, Text, TextInput, 
+    View, Text, TextInput, Alert,
     TouchableOpacity, StyleSheet 
 } from 'react-native'
 import { Link, router } from 'expo-router'
 import { useState } from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 
+import { auth } from '../../config'
 import Button from '../../components/Button'
 
-const handlePress = (): void => {
+const handlePress = (email: string, password: string): void => {
     // ログイン
-    router.replace('/memo/list')
+    console.log(email, password)
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            console.log(userCredential.user.uid)
+            router.replace('/memo/list')
+        })
+        .catch((error) => {
+            const {code, message} = error
+            console.log(code, message)
+            Alert.alert(message)
+        })
 }
 
 const LogIn = (): React.JSX.Element => {
@@ -37,10 +49,10 @@ const LogIn = (): React.JSX.Element => {
                     placeholder='Password'
                     textContentType='password'
                 />
-                <Button label='Submit' onPress={handlePress}/>
+                <Button label='Submit' onPress={() => handlePress(email, password)} />
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Not registered?</Text>
-                    <Link href='/auth/sign_up' asChild>
+                    <Link href='/auth/sign_up' asChild replace>
                         <TouchableOpacity>
                             <Text style={styles.footerLink}>Sign up here!</Text>
                         </TouchableOpacity>
--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -52,7 +52,7 @@ const SignUp = (): React.JSX.Element => {
                 <Button label='Submit' onPress={() => handlePress(email, password)} />
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Already registered?</Text>
-                    <Link href='/auth/log_in' asChild>
+                    <Link href='/auth/log_in' asChild replace>
                         <TouchableOpacity>
                             <Text style={styles.footerLink}>Log in.</Text>
                         </TouchableOpacity>
