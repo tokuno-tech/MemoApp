7d78384505971fa85ba1e0f8e36d7add0a064c6e
tokuno-tech <tokuno.web.work@gmail.com>
2025-06-05 22:47:05 +0900

    データの取得と監視まで

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,11 +1,13 @@
 import { View, StyleSheet } from 'react-native'
 import { router, useNavigation } from 'expo-router'
 import { useEffect } from 'react'
+import { collection, onSnapshot, query, orderBy} from 'firebase/firestore'
 
 import MemoListItem from '../../components/MemoListItem'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
 import LogOutButton from '../../components/LogOutButton'
+import { db, auth } from '../../config'
 
 const handlePress = (): void => {
     router.push('/memo/create')
@@ -19,6 +21,18 @@ const List = (): React.JSX.Element => {
                 return <LogOutButton />
             }
         })
+    },[])
+    useEffect(() => {
+        if (auth.currentUser === null) return
+        const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+        const q = query(ref, orderBy('updatedAt', 'desc'))
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            snapshot.forEach((doc) => {
+                console.log('memo', doc.data())
+            })
+        })
+        // このページが消えたタイミングで実行される
+        return unsubscribe
     },[])
     return (
         <View style={styles.container}>
