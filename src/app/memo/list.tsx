dc31017d2cf2bdd4a2d431da52f00d77604dab55
tokuno-tech <tokuno.web.work@gmail.com>
2025-06-17 23:29:17 +0900

    取得したデータを表示する

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,6 +1,6 @@
 import { View, StyleSheet } from 'react-native'
 import { router, useNavigation } from 'expo-router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { collection, onSnapshot, query, orderBy} from 'firebase/firestore'
 
 import MemoListItem from '../../components/MemoListItem'
@@ -8,12 +8,14 @@ import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
 import LogOutButton from '../../components/LogOutButton'
 import { db, auth } from '../../config'
+import { type Memo } from '../../../types/memo'
 
 const handlePress = (): void => {
     router.push('/memo/create')
 }
 
 const List = (): React.JSX.Element => {
+    const [memos, setMemos] = useState<Memo[]>([])
     const navigation = useNavigation()
     useEffect(() => {
         navigation.setOptions({
@@ -27,9 +29,17 @@ const List = (): React.JSX.Element => {
         const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
         const q = query(ref, orderBy('updatedAt', 'desc'))
         const unsubscribe = onSnapshot(q, (snapshot) => {
+            const remoteMemos: Memo[] = []
             snapshot.forEach((doc) => {
                 console.log('memo', doc.data())
+                const { bodyText, updatedAt } = doc.data()
+                remoteMemos.push({
+                    id: doc.id,
+                    bodyText,
+                    updatedAt
+                })
             })
+            setMemos(remoteMemos)
         })
         // このページが消えたタイミングで実行される
         return unsubscribe
@@ -37,9 +47,7 @@ const List = (): React.JSX.Element => {
     return (
         <View style={styles.container}>
             <View>
-                <MemoListItem />
-                <MemoListItem />
-                <MemoListItem />
+                {memos.map((memo) =>  <MemoListItem key={memo.id} memo={memo} />)}
             </View>
             <CircleButton onPress={handlePress}>
                 <Icon name='plus' size={40} color='#fff'/>
